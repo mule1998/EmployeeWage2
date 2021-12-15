e9b2eb31175db3a87cb00e0ae6aef5c054ff4858
Shubham Mule <shubhambmule@gmail.com>
2021-12-15 09:08:00 +0530

    Added : random function

--- employee_wage.py
+++ employee_wage.py
@@ -1,1 +1,8 @@
 print("Welcome to Employee Wage")
+import random
+"""using random function to generate possibilities"""
+rand=random.randint(0, 2)
+if rand==0:
+    print("Employee is present")
+else:
+    print("Emloyee is absent")
