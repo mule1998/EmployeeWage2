d77f17af1d55fb87f2a7addd8fe9e0a1e76d6a43
Shubham Mule <shubhambmule@gmail.com>
2021-12-15 20:33:57 +0530

    Added : part time

--- employee_wage.py
+++ employee_wage.py
@@ -1,13 +1,16 @@
 import random
 """Constants"""
-is_full_time=1
+is_full_time=2
+is_part_time=1
 emp_rate_per_hr=20
 emp_hrs=0
 emp_wage=0
 #computation
-rand=random.randint(0, 2)
+rand=random.randint(0, 3)
 if rand==is_full_time:
     emp_hrs=8
+elif rand==is_part_time:
+    emp_hrs=4
 else:
     emp_hrs=0
 emp_wage=emp_hrs*emp_rate_per_hr
