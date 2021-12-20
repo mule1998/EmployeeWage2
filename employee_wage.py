300f0877e16083da61c4ebc06d708f19317b0dcd
Shubham Mule <shubhambmule@gmail.com>
2021-12-20 18:58:19 +0530

    Added: Wage for multiple companies

--- employee_wage.py
+++ employee_wage.py
@@ -1,22 +1,21 @@
-"""Calculatig monthly employee wage"""
+"""Calculatig employee wage for multiple companies"""
 
 class EmpWage():
-    emp_rate_per_hr=20
-    days_in_month=20
-    hrs_in_month=100
+   
     is_full_time=8
     is_part_time=4
-    
     absent=0
-    def wage(self):
+    
+    def wage(self,company,hours,days,per_hr):
         work_hrs=0
         work_days=0 
-        salary=0
-    #computation
-        while work_days < self.days_in_month and work_hrs <= self.hrs_in_month :
+        emp_wage=0
+        emp_hrs=0
+        #computation
+        while work_days < days and work_hrs <= hours :
             work_days+=1
             import random
-            rand= random.randint(0, 3)
+            rand= random.randint(0, 2)
             if rand==0:
              work_hrs=self.is_full_time
             elif rand==1:
@@ -27,10 +26,11 @@ class EmpWage():
             if work_hrs>100:
                 work_hrs-=emp_hrs
                 break
-            salary=self.emp_rate_per_hr*work_hrs
-    print("Total Salary: " ,salary)
-    print("Total Hours: ", work_hrs )
+            emp_wage=per_hr*work_hrs
+        print("Total Salary: " ,emp_wage)
+        print("Total Hours: ", work_hrs )
+
 
 emp=EmpWage()
-emp.wage()
+emp.wage("BMW", 100, 20, 20)
 
